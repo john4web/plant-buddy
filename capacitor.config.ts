61cc1c50753c1051910e7185a10aaf8f984c5de5
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-12-09 19:35:26 +0100

    Adding Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.companyname.plantbuddy",
+  appName: "plant-buddy",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
