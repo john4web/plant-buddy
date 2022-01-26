23ef0f687669f3cfb9069b7d5da2cd4c68bdd135
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2022-01-27 00:19:03 +0100

    changing icons

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
         name: 'Plant Buddy',
-        themeColor: '#4DBA87',
+        themeColor: '#3b8686',
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
