de46d4189dd90ffae443eafc09f6739dbf3fb11b
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2022-01-27 17:06:19 +0100

    implement caching

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,3 @@
-// caching stuff
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return new Response(`Welcome to plant buddy!\n
-                You seem to be offline - check your connection!`);
-        })
-    );
-});
-
 // firebase cloud messaging stuff
 importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js');
@@ -31,4 +21,55 @@ messaging.onBackgroundMessage((payload) => {
     self.registration.showNotification(payload.data.title, {
         body: payload.data.body,
     });
+});
+
+let CACHE_NAME = "pb-cache";
+let CACHED_URLS = [
+    "/index.html",
+    "/favicon.ico",
+    "/firebase-messaging-sw.js",
+    "/manifest.json",
+    "/registerServiceWorker.ts",
+    "/robots.txt",
+    "/css/app.3c78f67a.css",
+    "/js/app.7923e42b.js",
+    "/js/chunk-vendors.b804dedf.js",
+    "/img/camera.1f6f1b8e.png",
+    "/img/plant_care_illustration.cd227edc.png",
+    "/img/plant-icon.021de54f.png",
+    "/img/icons/android-chrome-192x192.png",
+    "/img/icons/android-chrome-512x512.png",
+    "/img/icons/apple-touch-icon.png",
+    "/img/icons/favicon-16x16.png",
+    "/img/icons/favicon-32x32.png",
+    "/img/icons/mstile-150x150.png",
+    "/img/icons/safari-pinned-tab.svg"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(CACHED_URLS);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            //REquest ist schief gelaufen
+
+            //Gibt es das file im cache
+            return caches.match(event.request).then(function(response) {
+                if (response) {
+                    //if the file is in the cache -> return it
+                    return response;
+                } else {
+                    //if the file is not in the cache -> return offline.html
+                    console.log("Offline served");
+                   // return caches.match("/index-offline.html");
+                }
+            });
+        })
+    );
 });
